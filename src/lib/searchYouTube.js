c03c6797049b46899470239aefbd0902e8218af8
Gunpreet Singh <gunpreet@outlook.com>
2017-06-03 13:47:10 -0700

    add api key and psuedo code for search you tube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+  // pass in you tube api key, query string and max returned result and a call back 
+var searchYouTube = (key, query, maxResults, callback) => {
+// use jquerry get to start the request 
+    // pass in api url 
+    // pass in properties 
+        // part 
+        // key 
+        // query
+        // max result 
+        // type
+        // embeddable 
+    //on done, iterate through the result 
+        // u call back if passed in on all items 
+    // if fail , log response json 
+        // log each error 
+//esport you tube search function to the window 
 };
 
 window.searchYouTube = searchYouTube;
